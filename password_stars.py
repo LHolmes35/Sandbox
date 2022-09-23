e208f444087a696312ee5c4944ebf1098bbfaac6
LHolmes35 <114198658+LHolmes35@users.noreply.github.com>
2022-09-23 11:10:26 +1000

    Update password_stars.py

--- password_stars.py
+++ password_stars.py
@@ -2,7 +2,7 @@
 MINIMUM_LENGTH = 8
 
 user_password = input("Please input password: ")
-while len(user_password) < 8:
+while len(user_password) < MINIMUM_LENGTH:
     print("Invalid password")
     user_password = input("Please input password: ")
 for i in range(len(user_password)):
